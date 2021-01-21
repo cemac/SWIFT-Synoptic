87901a3f38c0ca8e19bee084eef80511f0a9bd0e
Tamora James <T.D.James1@leeds.ac.uk>
2021-01-21 16:57:23 +0000

    Add missing gridlines

--- synoptic/chart.py
+++ synoptic/chart.py
@@ -234,7 +234,7 @@ class SynopticChart:
         # Add gridlines
         grid_col = 'lightsteelblue'
         gl = ax.gridlines(draw_labels=True, x_inline=True, y_inline=True, zorder=1.5, color=grid_col)
-        gl.xlocator = mticker.FixedLocator(np.arange(-30, 30, 10))
+        gl.xlocator = mticker.FixedLocator(np.arange(-30, 40, 10))
         gl.ylocator = mticker.FixedLocator(np.arange(0, 40, 10))
         gl.xlabel_style = { 'color': grid_col}
         gl.ylabel_style = { 'color': grid_col}
