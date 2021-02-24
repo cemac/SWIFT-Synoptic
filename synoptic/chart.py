d010a26a0f92bfcf9403d3c9ee240d2354fcc329
Tamora James <T.D.James1@leeds.ac.uk>
2021-02-24 14:16:47 +0000

    Improve formatting of mid-level dry intrusion
    Replace previous mock up of WASA style with path patches applied to
    each segment of the contour at regular intervals.

--- synoptic/chart.py
+++ synoptic/chart.py
@@ -42,6 +42,8 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.cm as mcm
 import matplotlib.colors as mc
+import matplotlib.patches as mpatches
+import matplotlib.path as mpath
 from matplotlib.offsetbox import AnchoredText
 import cartopy.feature as cfeature
 import cartopy.crs as ccrs
@@ -865,17 +867,16 @@ class MidlevelDryIntrusion(SynopticComponent):
         self.units = 'percent'
         self.level_units = 'hPa'
 
-        self.levels = [ 58,
-                        60 ]
+        self.levels = [ 60 ]
 
         self.lw = 3.0
 
+        self.marker_thres = 0.8
+
         # Formatting options
         self.options = {
-            'linewidths': [ 2*self.lw,
-                            self.lw ],
-            'linestyles': [ (0, (0.9, 1.8)),
-                            'solid' ],
+            'linewidths': [ self.lw ],
+            'linestyles': [ 'solid' ],
             'colors': '#198dd5'
         }
 
@@ -889,6 +890,34 @@ class MidlevelDryIntrusion(SynopticComponent):
                          levels=self.levels,
                          **self.options)
         #ax.clabel(ctr, fmt = '%1.0f')
+
+        # Draw short line at right angles to line segments to indicate
+        # dry side of contour
+        for lc in ctr.collections:
+            segments = lc.get_segments()
+            for seg in segments:
+                dist_sum = self.marker_thres/2
+                for i, s in enumerate(seg[:-1]):
+                    # Check length of segment and add patches at
+                    # suitable intervals
+                    dx, dy = seg[i+1] - s
+                    seg_len = np.linalg.norm([dx, dy])
+                    dist_sum = dist_sum + seg_len
+                    while dist_sum > self.marker_thres:
+                        dist_sum -= self.marker_thres
+                        # Find start point for marker
+                        loc = (seg_len - dist_sum)/seg_len
+                        start = s + loc*np.array([dx, dy])
+                        # Find end point for marker
+                        v = np.array([-dy, dx])
+                        end = start + 0.33*v/np.linalg.norm(v)
+                        # Draw path
+                        pth = mpath.Path([start, end])
+                        line = mpatches.PathPatch(pth,
+                                                  color=self.options["colors"],
+                                                  linewidth=1.4*self.lw,
+                                                  capstyle='butt')
+                        ax.add_patch(line)
 
 class WindShear(WindComponent):
 
