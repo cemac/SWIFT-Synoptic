cemac/SWIFT-Synoptic

commit 5283fc7616c105ea38131bd160726a63e655d79f

Add MSLP to EA low-level chart
Partial fulfillment of #20.

synoptic/chart.py
@@ -344,6 +344,12 @@ class LowLevelChart(SynopticChart):
             self.wc_10m = WindHeightLevel(self, 10)
 
         elif self.domain_name == 'EA':
+v            # Mean sea level pressure
+            self.mslp = MeanSeaLevelPressure(self)
+            self.mslp.lw = 2.0
+            self.mslp.highlight = 1012
+            self.mslp.step = 4
+
             # 24 hour change in mean sea level pressure
             self.mslp_24 = MeanSeaLevelPressureChange(self)
 

commit e8514e06c8eee2b040029c2f74e6243812e56c27

Correct syntax error
Correct syntax error introduced in commit:5283fc76.

synoptic/chart.py
@@ -344,7 +344,7 @@ class LowLevelChart(SynopticChart):
             self.wc_10m = WindHeightLevel(self, 10)
 
         elif self.domain_name == 'EA':
-v            # Mean sea level pressure
+            # Mean sea level pressure
             self.mslp = MeanSeaLevelPressure(self)
             self.mslp.lw = 2.0
             self.mslp.highlight = 1012